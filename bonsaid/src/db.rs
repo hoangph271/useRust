bd6932beb600b1ed82123a5354570500d6f369ac
Phan Huy Hoang <hoangph271@gmail.com>
2022-03-31 04:52:39 +0700

    [CRUD]

--- bonsaid/src/db.rs
+++ bonsaid/src/db.rs
@@ -1,28 +1,31 @@
 use bonsaidb::core::document::{CollectionDocument, Emit};
 use bonsaidb::core::schema::view::CollectionViewSchema;
 use bonsaidb::core::schema::{Collection, ReduceResult, View, ViewMapResult, ViewMappedValue};
-use bonsaidb::local::{
-    config::{Builder, StorageConfiguration},
-    Database,
-};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Collection)]
-#[collection(name = "shapes", views = [PostsCount])]
+#[collection(name = "posts", views = [PostsCount])]
 pub struct Post {
-    pub id: u32,
     pub title: String,
 }
 
+impl Default for Post {
+    fn default() -> Self {
+        Self {
+            title: "Untitled".to_owned(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, View)]
-#[view(collection = Post, key = u32, value = usize, name = "posts-count")]
+#[view(collection = Post, key = u64, value = usize, name = "posts-count")]
 pub struct PostsCount;
 
 impl CollectionViewSchema for PostsCount {
     type View = Self;
 
     fn map(&self, document: CollectionDocument<Post>) -> ViewMapResult<Self::View> {
-        document.header.emit_key_and_value(document.contents.id, 1)
+        document.header.emit_key_and_value(document.header.id, 1)
     }
 
     fn reduce(
@@ -34,11 +37,61 @@ impl CollectionViewSchema for PostsCount {
     }
 }
 
-#[test]
-fn test_can_run() {}
+// * TESTS
 
-pub async fn can_create_posts() -> Result<(), bonsaidb::core::Error> {
-    let db = Database::open::<Post>(StorageConfiguration::new("view-examples.bonsaidb")).await;
+#[allow(dead_code)]
+fn create_post_db() -> Result<bonsaidb::local::Database, bonsaidb::core::Error> {
+    use bonsaidb::local::{
+        config::{Builder, StorageConfiguration},
+        Database,
+    };
+
+    let db = Database::open::<Post>(StorageConfiguration::new("").memory_only())?;
+
+    Ok(db)
+}
+
+#[test]
+fn can_create_post_db() {
+    create_post_db().unwrap();
+}
+
+#[test]
+fn can_create_posts() -> Result<(), bonsaidb::core::Error> {
+    use bonsaidb::core::schema::SerializedCollection;
+    let db = create_post_db()?;
+
+    let post = Post::default().push_into(&db)?;
+
+    let db_post = Post::get(post.header.id, &db)?.expect("couldn't retrieve stored item");
+
+    assert_eq!(db_post.header.id, post.header.id);
+
+    Ok(())
+}
+
+#[test]
+fn can_update_posts() -> Result<(), bonsaidb::core::Error> {
+    use bonsaidb::core::schema::SerializedCollection;
+    let db = create_post_db()?;
+
+    let mut post = Post::default().push_into(&db)?;
+
+    post.contents.title = "Updated".to_owned();
+
+    assert_eq!(post.contents.title, "Updated");
+
+    Ok(())
+}
+
+#[test]
+fn can_delete_posts() -> Result<(), bonsaidb::core::Error> {
+    use bonsaidb::core::schema::SerializedCollection;
+    let db = create_post_db()?;
+
+    let post = Post::default().push_into(&db)?;
+
+    post.delete(&db)?;
 
     Ok(())
 }
--- bonsaid/src/main.rs
+++ bonsaid/src/main.rs
@@ -1,9 +1,3 @@
 mod db;
 
-// TODO:
-// - Async runtime...?
-// - C.R.U.D Posts
-
-fn main() {
-    futures::executor::block_on(db::can_create_posts()).unwrap();
-}
+fn main() {}
