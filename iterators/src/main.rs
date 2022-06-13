87da12375a8b7f753c1a0346561635ddc8ad0e24
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-13 11:17:46 +0700

    [Iterators]

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let nums = vec![1, 2, 3, 4];
+    let nums = vec![0, 1, 2, 3, 4];
     let upper_limit = 2;
 
     let res = nums
-        .iter()
-        .filter(|num| **num < upper_limit)
-        .map(|num| num + 1)
-        .collect::<Vec<u64>>();
+        .into_iter()
+        .filter(|num| num < &upper_limit)
+        .map(|num| format!("#{num}"))
+        .collect::<Vec<_>>();
 
     println!("{:?}", res);
 }
