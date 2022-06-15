86b754b7dba0043754451e7d287d7d33a46cefaa
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-15 13:17:10 +0700

    [Add] threading/rw_lock

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,9 +1,11 @@
 mod arc_mutext;
 mod channels;
 mod threads;
+mod rw_lock;
 
 fn main() {
     // arc_mutext::arc_mutex();
     // threads::thread_spawn();
-    channels::channels();
+    // channels::channels();
+    rw_lock::rw_lock();
 }
---
+++ threading/src/rw_lock.rs
@@ -0,0 +1,39 @@
+use std::sync::{Arc, RwLock};
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
+pub fn rw_lock() {
+    let mutex = RwLock::new(1);
+    let arced_mutext = Arc::new(mutex);
+
+    let thread_ids = (0..10).collect::<Vec<_>>();
+    let threads = thread_ids.iter().map(|i| {
+        let mutex_clone = arced_mutext.clone();
+        let thread_id = i.clone();
+
+        spawn(move || {
+            // ? This is to preven dead lock
+            {
+                let mut guard = mutex_clone.write().unwrap();
+                *guard += 1;
+                println!("{}", *guard);
+            }
+            // * This will work as well
+            // drop(guard);
+
+            if thread_id % 2 == 0 {
+                sleep(Duration::from_millis(100));
+
+                let mut guard = mutex_clone.write().unwrap();
+                *guard += thread_id;
+            }
+        })
+    });
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    let final_result = arced_mutext.read().unwrap();
+    println!("Finally: {final_result}");
+}
