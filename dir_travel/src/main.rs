22bd29fe63afc0f46530eed122ff43631a7fe4e9
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-17 16:31:09 +0700

    [Add] Init

---
+++ dir_travel/src/main.rs
@@ -0,0 +1,7 @@
+// TODO: Singled threaded vs multiple threaded...!
+// TODO: Benchmark 'em functions
+// TODO: Ignore `.*` directories when traveling
+
+fn main() {
+    println!("Hello, world!");
+}
