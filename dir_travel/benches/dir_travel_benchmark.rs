30e0612f76743a4a33b796e0de616a0e9d54e306
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-20 09:59:47 +0700

    [Add] dir_travel_benchmark.rs

--- dir_travel/benches/dir_travel_benchmark.rs
+++ dir_travel/benches/dir_travel_benchmark.rs
@@ -1,12 +1,16 @@
-// use crate::handlers;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+#[path = "../src/handlers/handlers.rs"]
+mod handlers;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::path::Path;
 
 // TODO: Benchmark 'em functions
 
 fn single_threaded_travel_benchmark(criterion: &mut Criterion) {
-    // criterion.bench_function("single_threaded_travel_benchmark", |bencher| {
-    //     bencher.iter(|| )
-    // })
+    criterion.bench_function("single_threaded_travel_benchmark", |bencher| {
+        bencher.iter(|| {
+            handlers::single_threaded_travel(&Path::new("."));
+        });
+    });
 }
 
 criterion_group!(benches, single_threaded_travel_benchmark);
